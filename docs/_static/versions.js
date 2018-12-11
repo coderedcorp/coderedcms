42065087d9cf737e5bc0a5fad55700e73f2196e5
Vince Salvino <salvino@coderedcorp.com>
2018-12-11 10:33:17 -0500

    Update to docs versions.js to cache versions locally in browser to avoid ajax calls

--- docs/_static/versions.js
+++ docs/_static/versions.js
@@ -1,15 +1,27 @@
-$(document).ready(function() {
-    $.ajax({
-        url: '/cms/versions.txt',
-        success: function(data) {
-            data.split('\n').forEach((item, index) => {
-                if(item.trim() != '') {
-                    newa = document.createElement('a', );
-                    newa.setAttribute('href', '/cms/' + item + '/');
-                    newa.innerHTML = item;
-                    document.getElementById("other-versions").appendChild(newa);
-                }
-            }
-        )}
+vfile = '/cms/versions.txt';
+
+function setversions(data) {
+    data.split('\n').forEach((item, index) => {
+        if(item.trim() != '') {
+            newa = document.createElement('a', );
+            newa.setAttribute('href', '/cms/' + item + '/');
+            newa.innerHTML = item;
+            document.getElementById("other-versions").appendChild(newa);
+        }
     });
+}
+
+$(document).ready(function() {
+    if(sessionStorage.getItem(vfile)) {
+        setversions(sessionStorage.getItem(vfile));
+    }
+    else {
+        $.ajax({
+            url: '/cms/versions.txt',
+            success: function(data) {
+                sessionStorage.setItem(vfile, data);
+                setversions(data);
+            }
+        });
+    }
 });

d4950de7ea159157abb38c464c0cf5474f422b71
Vince Salvino <salvino@coderedcorp.com>
2019-01-22 19:46:11 -0500

    Version number increment

--- coderedcms/__init__.py
+++ coderedcms/__init__.py
@@ -1,4 +1,4 @@
-release = ['0', '10', '0']
+release = ['0', '11', '0']
 
 __version__ = "{0}.{1}.{2}".format(release[0], release[1], release[2])
 __shortversion__ = "{0}.{1}".format(release[0], release[1])

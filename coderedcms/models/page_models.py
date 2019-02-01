f595b2d0f2be8ce6b0c0fec15e2d686f5567b86a
Cory Sutyak <sutyak@coderedcorp.com>
2019-02-01 14:46:28 -0500

    Fixed default ordering

--- coderedcms/models/page_models.py
+++ coderedcms/models/page_models.py
@@ -135,7 +135,7 @@ class CoderedPage(Page, metaclass=CoderedPageMeta):
     # ordering based on specific subpage fields.
     index_order_by_default = 'default'
     index_order_by_choices = (
-        ('default', _('Default Ordering')),
+        ('', _('Default Ordering')),
         ('-first_published_at', _('Date first published, newest to oldest')),
         ('first_published_at', _('Date first published, oldest to newest')),
         ('-last_published_at', _('Date updated, newest to oldest')),
@@ -152,6 +152,7 @@ class CoderedPage(Page, metaclass=CoderedPageMeta):
         choices=index_order_by_choices,
         default=index_order_by_default,
         verbose_name=_('Order child pages by'),
+        blank=True,
     )
     index_num_per_page = models.PositiveIntegerField(
         default=10,
@@ -482,10 +483,9 @@ class CoderedPage(Page, metaclass=CoderedPageMeta):
         """
         Override to return query of subpages as defined by `index_` variables.
         """
-        if self.index_query_pagemodel and self.index_order_by != 'default':
+        if self.index_query_pagemodel and self.index_order_by:
             querymodel = resolve_model_string(self.index_query_pagemodel, self._meta.app_label)
             return querymodel.objects.child_of(self).order_by(self.index_order_by)
-
         return super().get_children().live()
 
     def get_content_walls(self, check_child_setting=True):

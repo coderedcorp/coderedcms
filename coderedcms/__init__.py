1b397133cd886cfaa3f5bf199f64d01972fbe7f0
Vince Salvino <salvino@coderedcorp.com>
2019-04-04 12:21:45 -0400

    0.13.2 release

--- coderedcms/__init__.py
+++ coderedcms/__init__.py
@@ -1,4 +1,4 @@
-release = ['0', '13', '1']
+release = ['0', '13', '2']
 
 __version__ = "{0}.{1}.{2}".format(release[0], release[1], release[2])
 __shortversion__ = "{0}.{1}".format(release[0], release[1])

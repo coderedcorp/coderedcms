39e2b9d3b44f53441fb58c6c701a42a1840e033e
Cory Sutyak <sutyak@coderedcorp.com>
2019-05-07 11:39:04 -0400

    Refactored tests.

--- coderedcms/models/tests/test_page_models.py
+++ coderedcms/models/tests/test_page_models.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.models import AnonymousUser
 from django.test import TestCase
 from django.test.client import RequestFactory
 from wagtail.core.models import Site
+from unittest import skip
 
 from coderedcms.models.page_models import (
     CoderedArticleIndexPage,
@@ -18,18 +20,9 @@ from coderedcms.tests.testapp.models import (
 )
 
 
-class NotCreatablePageTestCase():
-
-    def test_not_available(self):
-        self.assertFalse(self.model.is_creatable)
-        self.assertFalse(self.model in get_page_models())
-
-
-class CreatablePageTestCase():
-
-    def test_is_available(self):
-        self.assertTrue(self.model.is_creatable)
-        self.assertTrue(self.model in get_page_models())
+class BasicPageTestCase():
+    class Meta:
+        abstract=True
 
     def setUp(self):
         self.request_factory = RequestFactory()
@@ -39,25 +32,48 @@ class CreatablePageTestCase():
         self.homepage = WebPage.objects.get(url_path='/home/')
         self.homepage.add_child(instance=self.basic_page)
 
-    def test_serve(self):
-        request = self.request_factory.post(self.basic_page.url)
+    def test_get(self):
+        request = self.request_factory.get(self.basic_page.url)
+        request.user = AnonymousUser()
+        request.site = Site.objects.all()[0]
         response = self.basic_page.serve(request)
         self.assertEqual(response.status_code, 200)
 
+class AbstractPageTestCase():
+    class Meta:
+        abstract=True
 
-class CoderedArticleIndexPageTestCase(NotCreatablePageTestCase, TestCase):
+    def test_not_available(self):
+        self.assertFalse(self.model.is_creatable)
+        self.assertFalse(self.model in get_page_models())
+
+
+class ConcretePageTestCase():
+    class Meta:
+        abstract=True
+
+    def test_is_available(self):
+        self.assertTrue(self.model.is_creatable)
+        self.assertTrue(self.model in get_page_models())
+
+
+class ConcreteBasicPageTestCase(ConcretePageTestCase, BasicPageTestCase):
+    class Meta:
+        abstract=True
+
+class CoderedArticleIndexPageTestCase(AbstractPageTestCase, TestCase):
     model = CoderedArticleIndexPage
 
 
-class CoderedArticlePageTestCase(NotCreatablePageTestCase, TestCase):
+class CoderedArticlePageTestCase(AbstractPageTestCase, TestCase):
     model = CoderedArticlePage
 
 
-class CoderedFormPageTestCase(NotCreatablePageTestCase, TestCase):
+class CoderedFormPageTestCase(AbstractPageTestCase, TestCase):
     model = CoderedFormPage
 
 
-class CoderedPageTestCase(NotCreatablePageTestCase, TestCase):
+class CoderedPageTestCase(TestCase):
     model = CoderedPage
 
     def test_not_available(self):
@@ -65,21 +81,21 @@ class CoderedPageTestCase(NotCreatablePageTestCase, TestCase):
         self.assertTrue(self.model in get_page_models())
 
 
-class CoderedWebPageTestCase(NotCreatablePageTestCase, TestCase):
+class CoderedWebPageTestCase(AbstractPageTestCase, TestCase):
     model = CoderedWebPage
 
 
-class ArticlePageTestCase(CreatablePageTestCase, TestCase):
+class ArticlePageTestCase(ConcreteBasicPageTestCase, TestCase):
     model = ArticlePage
 
 
-class ArticleIndexPageTestCase(CreatablePageTestCase, TestCase):
+class ArticleIndexPageTestCase(ConcreteBasicPageTestCase, TestCase):
     model = ArticleIndexPage
 
 
-class FormPageTestCase(CreatablePageTestCase, TestCase):
+class FormPageTestCase(ConcreteBasicPageTestCase, TestCase):
     model = FormPage
 
 
-class WebPageTestCase(CreatablePageTestCase, TestCase):
+class WebPageTestCase(ConcreteBasicPageTestCase, TestCase):
     model = WebPage

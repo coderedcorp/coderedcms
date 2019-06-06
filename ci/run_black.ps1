85b8dc720c9fd0531a1eda75da1f775c660df5db
Kevin Cummings <cummings@coderedcorp.com>
2019-06-06 11:08:43 -0400

    2 Jobs on Azure

--- ci/run_black.ps1
+++ ci/run_black.ps1
@@ -2,12 +2,12 @@ $ExitCode = 0
 $GitDiff = git diff origin/master
 # If there is no diff between master, then black everything.
 if ( $GitDiff -eq $null ) {
-    black --diff .
+    black .
     if ($LastExitCode -ne 0) { $ExitCode = $LastExitCode }
 }
 # Else black just the diff.
 else {
-    Write-Output $GitDiff | black --diff
+    Write-Output $GitDiff | black
     if ($LastExitCode -ne 0) { $ExitCode = $LastExitCode }
     # If the project_template changed, then black the testproject too.
     $GitDiffTempl = Write-Output $GitDiff | Select-String -Pattern "^diff .*/project_template/.*"

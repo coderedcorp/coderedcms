be4ae09679a36dde9e20bf800636a9e00c24393f
Kevin Cummings <cummings@coderedcorp.com>
2019-06-06 13:02:27 -0400

    2 Jobs

--- ci/run_black.ps1
+++ ci/run_black.ps1
@@ -1,18 +1,22 @@
+#
+# Use PowerShell Core for any scripts so they are re-usable across windows/mac/linux.
+#
+
 $ExitCode = 0
 $GitDiff = git diff origin/master
 # If there is no diff between master, then black everything.
 if ( $GitDiff -eq $null ) {
-    black .
+    black --diff .
     if ($LastExitCode -ne 0) { $ExitCode = $LastExitCode }
 }
 # Else black just the diff.
 else {
-    Write-Output $GitDiff | black
+    Write-Output $GitDiff | black --diff
     if ($LastExitCode -ne 0) { $ExitCode = $LastExitCode }
     # If the project_template changed, then black the testproject too.
     $GitDiffTempl = Write-Output $GitDiff | Select-String -Pattern "^diff .*/project_template/.*"
     if ( $GitDiffTempl -ne $null ) {
-        black .
+        black --diff .
         if ($LastExitCode -ne 0) { $ExitCode = $LastExitCode }
     }
 }

78dc407f701caee76f39e1c221de33e0ccbd0279
Kevin <kevincummings3@outlook.com>
2019-07-17 16:59:50 -0400

    Percentages in Code Coverage

--- ci/run_artifacts.ps1
+++ ci/run_artifacts.ps1
@@ -7,13 +7,19 @@ if (Test-Path -Path "/home/vsts/work/artifacts/Code Coverage Report_*/summary*/c
 
 [xml]$BranchXML = Get-Content .\coverage.xml
 
-$masterlinerate = $MasterXML.coverage.'line-rate'
-$branchlinerate = $BranchXML.coverage.'line-rate'
+$masterlinerate = [math]::Abs([math]::Round([decimal]$MasterXML.coverage.'line-rate' * 100, 2))
+$branchlinerate = [math]::Abs([math]::Round([decimal]$BranchXML.coverage.'line-rate' * 100, 2))
 
-Write-Output "Old line coverage rate: $masterlinerate"
-Write-Output "New line coverage rate: $branchlinerate"
+$masterlinerate = 59.50
 
-$change = [math]::Abs([math]::Round((($branchlinerate - $masterlinerate) / $masterlinerate) * 100, 2))
+Write-Output "Old line coverage rate: $masterlinerate%"
+Write-Output "New line coverage rate: $branchlinerate%"
+
+if ($masterlinerate -ne 0) {
+    $change = [math]::Abs([math]::Round((($branchlinerate - $masterlinerate) / $masterlinerate) * 100, 2))
+} else {
+    $change = "Infinite"
+}
 
 if ($branchlinerate -gt $masterlinerate) {
     Write-Host "Code coverage has increased by $change%. Build passed." -ForegroundColor Green

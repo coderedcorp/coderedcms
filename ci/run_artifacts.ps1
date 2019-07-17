21db6439bcb6f7dc0d67d2090691b95a923bbe75
Kevin <kevincummings3@outlook.com>
2019-07-17 11:06:38 -0400

    Code Coverage Comparison

--- ci/run_artifacts.ps1
+++ ci/run_artifacts.ps1
@@ -10,9 +10,13 @@ $newoutput = -join("New line coverage rate: ", $newlinerate)
 Write-Output $oldoutput
 Write-Output $newoutput
 
-if ($newlinerate -ge $oldlinerate) {
+if ($newlinerate -gt $oldlinerate) {
+    Write-Output "Code coverage has increased. Build passed."
+    exit 0
+} else if ($newlinerate -eq $oldlinerate) {
+    Write-Output "Code coverage has not changed. Build passed."
     exit 0
 } else {
-    Write-Error "Code coverage as decreased. Build failed."
+    Write-Error "Code coverage as decreased. Code coverage must be greater than or equal to the previous build to pass."
     exit 1
 }

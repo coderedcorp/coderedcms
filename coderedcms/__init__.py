7282b8ddbb0dfdbd73753fb536c3f9cba0173c85
Vince Salvino <salvino@coderedcorp.com>
2020-08-08 18:06:17 -0400

    Release 0.19.0

--- coderedcms/__init__.py
+++ coderedcms/__init__.py
@@ -14,7 +14,7 @@ Maintains version of coderedcms.
 
 See: https://www.python.org/dev/peps/pep-0440/
 """
-release = ["0", "19", "0", "rc1", ""]
+release = ["0", "19", "0", "", ""]
 
 
 def _get_version() -> str:

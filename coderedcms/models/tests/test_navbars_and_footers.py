9a602524a8b948a4c2024bf59dfbed9478b83a14
Roxanna Coldiron <roxanna@coderedcorp.com>
2021-04-08 14:13:30 -0400

    migration test cases WIP

--- coderedcms/models/tests/test_navbars_and_footers.py
+++ coderedcms/models/tests/test_navbars_and_footers.py
@@ -1,28 +1,44 @@
 # what imports do I need?
 
-# from django.apps import apps
-# from django.test import Client, TestCase
-# from django.db.migrations.executor import MigrationExecutor
-# from django.db import connection
+from django.apps import apps
+from django.test import Client, TestCase
+from django.db.migrations.executor import MigrationExecutor
+from django.db import connection
 
-# from wagtail.core.models import Site
+from wagtail.core.models import Site
 
-# from coderedcms.models.wagtailsettings_models import (
-#     LayoutSettings,
-#     NavbarOrderable,
-#     FooterOrderable
-# )
+from coderedcms.models.wagtailsettings_models import (
+    LayoutSettings,
+    NavbarOrderable,
+    FooterOrderable
+)
 
 # Set up
 
-# class TestMigrations(TestCase):
+class TestMigrations(TestCase):
 
-#     @property
-#     def app(self):
-#         return apps.get_containing_app_config(type(self).__module__).name
+    @property
+    def app(self):
+        return apps.get_containing_app_config(type(self).__module__).name
 
-#     migrate_from = None
-#     migrate_to = None
+    migrate_from = None
+    migrate_to = None
 
 # Need to create Site that has navbar and footer the current way
-# Then run the migration and check if the navbar and footer was added to orderables
+class NavsandFootersTestCase(TestMigrations):
+
+    def setUpBeforeMigration(self, apps):
+        NavbarInitial = Navbar.objects.create(
+            name = "Main Nav",
+            menu_items = # how to add streamfield items here??,
+
+        )
+        FooterInitial = Footer.objects.create(
+            name = "Main Footer",
+            content = # how to add streamfield items here??,
+        )
+
+
+
+
+# Then run the migrations and check if the navbar and footer was added to orderables

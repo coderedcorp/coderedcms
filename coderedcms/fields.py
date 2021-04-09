7ea27f9e4cf791fc7cf6b89e82ad8baa1de842ab
Roxanna Coldiron <roxanna@coderedcorp.com>
2021-04-09 16:01:33 -0400

    new ScriptField and specified the attributes and style of the widget

--- coderedcms/fields.py
+++ coderedcms/fields.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from coderedcms.widgets import ColorPickerWidget
+from django.forms.widgets import Textarea
 
 
 class ColorField(models.CharField):
@@ -11,3 +12,15 @@ class ColorField(models.CharField):
     def formfield(self, **kwargs):
         kwargs['widget'] = ColorPickerWidget
         return super(ColorField, self).formfield(**kwargs)
+
+class ScriptField(models.TextField):
+    def __init__(self, *args, **kwargs):
+        super(ScriptField, self).__init__(*args, **kwargs)
+
+    def formfield(self, **kwargs):
+        kwargs['widget'] = Textarea(attrs={
+            'rows': 15,
+            'cols': 10,
+            'style': 'font-family:SFMono-Regular,Menlo,Monaco,Consolas,monospace;'
+        })
+        return super(ScriptField, self).formfield(**kwargs)
--- coderedcms/models/wagtailsettings_models.py
+++ coderedcms/models/wagtailsettings_models.py
@@ -12,6 +12,7 @@ from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.contrib.settings.models import BaseSetting, register_setting
 from wagtail.images import get_image_model_string
 
+from coderedcms.fields import ScriptField
 from coderedcms.settings import cr_settings
 
 
@@ -226,13 +227,13 @@ class AnalyticsSettings(BaseSetting):
         verbose_name=_('Track button clicks'),
         help_text=_('Track all button clicks using Google Analytics event tracking. Event tracking details can be specified in each button’s advanced settings options.'),  # noqa
     )
-    head_scripts = models.TextField(
+    head_scripts = ScriptField(
         blank=True,
         null=True,
         verbose_name=_('<head> tracking scripts'),
         help_text=_('Add tracking scripts between the <head> tags.'),
     )
-    body_scripts = models.TextField(
+    body_scripts = ScriptField(
         blank=True,
         null=True,
         verbose_name=_('<body> tracking scripts'),

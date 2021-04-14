c24ca5ab2d486d8854f5ec90b1c2dcb9644d4aae
Roxanna Coldiron <roxanna@coderedcorp.com>
2021-04-14 16:47:00 -0400

    WIP tests

--- coderedcms/models/tests/test_navbars_and_footers.py
+++ coderedcms/models/tests/test_navbars_and_footers.py
@@ -1,7 +1,10 @@
 # what imports do I need?
+# possibly use https://pypi.org/project/django-test-migrations/ instead?
 
+import json
 from django.apps import apps
 from django.test import Client, TestCase
+# from django_test_migrations.contrib.unittest_case import MigratorTestCase
 from django.db.migrations.executor import MigrationExecutor
 from django.db import connection
 
@@ -13,8 +16,8 @@ from coderedcms.models.wagtailsettings_models import (
     FooterOrderable
 )
 
-# Set up
 
+# Set up
 class TestMigrations(TestCase):
 
     @property
@@ -24,18 +27,23 @@ class TestMigrations(TestCase):
     migrate_from = None
     migrate_to = None
 
+
 # Need to create Site that has navbar and footer the current way
 class NavsandFootersTestCase(TestMigrations):
 
-    def setUpBeforeMigration(self, apps):
+    def prepare(self, apps):
         NavbarInitial = Navbar.objects.create(
             name = "Main Nav",
-            menu_items = # how to add streamfield items here??,
+            menu_items = json.dumps[
+                {\"type\": \"row\", \"value\": {\"settings\": {\"custom_template\": \"\", \"custom_css_class\": \"\", \"custom_id\": \"\"}, \"fluid\": false, \"content\": [{\"type\": \"content\", \"value\": {\"settings\": {\"custom_template\": \"\", \"custom_css_class\": \"\", \"custom_id\": \"\", \"column_breakpoint\": \"md\"}, \"column_size\": \"\", \"content\": [{\"type\": \"text\", \"value\": \"<h2>Footer Content</h2>\", \"id\": \"5d6be173-bab8-4d2f-93a9-94614267e776\"}]}, \"id\": \"016bdac5-0b6c-484f-ae53-b9ae115d5dd9\"}]}, \"id\": \"0d39786b-e959-4382-898a-20946f979da1\"}
+            ]
 
         )
         FooterInitial = Footer.objects.create(
             name = "Main Footer",
-            content = # how to add streamfield items here??,
+            content = json.dumps[
+                {\"type\": \"row\", \"value\": {\"settings\": {\"custom_template\": \"\", \"custom_css_class\": \"\", \"custom_id\": \"\"}, \"fluid\": false, \"content\": [{\"type\": \"content\", \"value\": {\"settings\": {\"custom_template\": \"\", \"custom_css_class\": \"\", \"custom_id\": \"\", \"column_breakpoint\": \"md\"}, \"column_size\": \"\", \"content\": [{\"type\": \"text\", \"value\": \"<h2>Footer Content</h2>\", \"id\": \"5d6be173-bab8-4d2f-93a9-94614267e776\"}]}, \"id\": \"016bdac5-0b6c-484f-ae53-b9ae115d5dd9\"}]}, \"id\": \"0d39786b-e959-4382-898a-20946f979da1\"}
+            ]
         )
 
 

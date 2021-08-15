7a83e3c2f7ea5c23e717b537695640b3833513c1
Vince Salvino <salvino@coderedcorp.com>
2021-08-15 18:03:58 -0400

    Update trove classifiers

--- setup.py
+++ setup.py
@@ -33,7 +33,6 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3 :: Only',
         'Framework :: Django',
-        'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
         'Framework :: Django :: 3.1',
         'Framework :: Django :: 3.2',

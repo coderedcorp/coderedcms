d5667d2106f51b5ef5623355a902ba5f90d7543c
Vince Salvino <salvino@coderedcorp.com>
2021-09-23 17:53:26 -0400

    0.22.2 version increment

--- coderedcms/__init__.py
+++ coderedcms/__init__.py
@@ -14,7 +14,7 @@ Maintains version of coderedcms.
 
 See: https://www.python.org/dev/peps/pep-0440/
 """
-release = ["0", "22", "1", "", ""]
+release = ["0", "22", "2", "", ""]
 
 
 def _get_version() -> str:

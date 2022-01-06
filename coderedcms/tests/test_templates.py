71ba04a00fc3c085d2a6ceead5aa8ea5f05d51c1
James Osgood <james@arrai.com>
2022-01-06 11:11:33 -0700

    Adjust tests to reduce line length.

--- coderedcms/tests/test_templates.py
+++ coderedcms/tests/test_templates.py
@@ -6,6 +6,12 @@ from django.test import TestCase
 
 from coderedcms.templatetags.coderedcms_tags import cr_settings
 
+EXPECTED_BANNER_HTML = """
+<div class="codered-banner" style="background-color:#f00; color:#fff; width:100%; padding:4px;">
+    Test
+</div>
+"""
+
 
 @pytest.mark.django_db
 class TestSiteBanner(TestCase):
@@ -14,11 +20,7 @@ class TestSiteBanner(TestCase):
     def test_with_banner(self):
         response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertInHTML(
-            '<div class="codered-banner" style="background-color:#f00; color:#fff; width:100%; padding:4px;">'
-            "Test</div>",
-            response.content.decode("utf-8")
-        )
+        self.assertInHTML(EXPECTED_BANNER_HTML, response.content.decode("utf-8"))
 
     def test_without_banner(self):
         response = self.client.get("/")
@@ -29,7 +31,11 @@ class TestSiteBanner(TestCase):
 @pytest.mark.django_db
 class TestWagtailAdminBanner(TestCase):
     def setUp(self):
-        admin = get_user_model().objects.create_superuser("admin", email="admin@example.com", password="admin")
+        admin = get_user_model().objects.create_superuser(
+            "admin",
+            email="admin@example.com",
+            password="admin"
+        )
         self.client.force_login(admin)
 
     def tearDown(self):
@@ -39,11 +45,7 @@ class TestWagtailAdminBanner(TestCase):
     def test_with_banner(self):
         response = self.client.get("/admin/")
         self.assertEqual(response.status_code, 200)
-        self.assertInHTML(
-            '<div class="codered-banner" style="background-color:#f00; color:#fff; width:100%; padding:4px;">'
-            "Test</div>",
-            response.content.decode("utf-8")
-        )
+        self.assertInHTML(EXPECTED_BANNER_HTML, response.content.decode("utf-8"))
 
     def test_without_banner(self):
         response = self.client.get("/admin/")

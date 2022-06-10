e87995488f4593f75b3dea6461728710bb0b4de6
Vince Salvino <salvino@coderedcorp.com>
2022-06-09 20:19:32 -0400

    Bump to version 0.24.0

--- coderedcms/__init__.py
+++ coderedcms/__init__.py
@@ -14,7 +14,7 @@ Maintains version of coderedcms.
 
 See: https://www.python.org/dev/peps/pep-0440/
 """
-release = ["0", "24", "0", "", "dev0"]
+release = ["0", "24", "0", "", ""]
 
 
 def _get_version() -> str:

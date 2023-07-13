e318a5e1e79eaf3631138b5864658fde0f30384f
Jeremy <jchilders@coderedcorp.com>
2023-07-13 15:20:03 -0400

    Bug: Correct length of classifier term length to 255

--- coderedcms/models/snippet_models.py
+++ coderedcms/models/snippet_models.py
@@ -189,6 +189,7 @@ class ClassifierTerm(Orderable, models.Model):
         allow_unicode=True,
         unique=True,
         verbose_name=_("Slug"),
+        max_length=255,
     )
     name = models.CharField(
         max_length=255,

650e43f06baba6fcad4bc078bcd5591f487dd10a
Jayden Kneller <jayden@arrai.com>
2023-07-18 15:21:00 -0600

    fix: Add code to deal with htmlvalue.

--- coderedcms/comparisons/snippet_comparisons.py
+++ coderedcms/comparisons/snippet_comparisons.py
@@ -8,6 +8,7 @@ from wagtail.admin.compare import (
 )
 
 from wagtail.admin.panels import get_edit_handler
+from wagtail.snippets.blocks import SnippetChooserBlock
 
 
 class ReusableContentComparison(BlockComparison):
@@ -20,6 +21,78 @@ class ReusableContentComparison(BlockComparison):
         revision = get_object_or_404(instance.revisions, id=revision_id)
         return revision.as_object()
 
+    def _get_revisions_diff(self):
+        instance = self.val_a["content"]
+        revision_a = self.get_revision(self.val_a)
+        revision_b = self.get_revision(self.val_b)
+
+        # Copied from with modifications:
+        # https://github.com/wagtail/wagtail/blob/
+        # 9edaadaef094d7732025121c32313472b353ace8/
+        # wagtail/admin/views/generic/models.py#L739-L752
+        comparison = (
+            get_edit_handler(self.val_a["content"])
+            .get_bound_panel(instance=instance)
+            .get_comparison()
+        )
+
+        htmldiffs = []
+        for comp in comparison:
+            diff = comp(revision_a, revision_b)
+            htmldiffs.append(
+                (
+                    diff.field_label(),
+                    diff.htmldiff(),
+                )
+            )
+
+        return (
+            # Use "Reusable Content" instead or "Content",
+            # for a better looking diff.
+            instance._meta.verbose_name,
+            format_html(
+                "<dl>\n{}\n</dl>",
+                format_html_join(
+                    "\n",
+                    "    <dt>{}</dt>\n    <dd>{}</dd>",
+                    htmldiffs,
+                ),
+            ),
+        )
+
+    def htmlvalue(self, val):
+        htmlvalues = []
+        for name, block in self.block.child_blocks.items():
+            label = self.block.child_blocks[name].label
+
+            if name == "revision":
+                # Ignore this field, we don't want to display diffs for it.
+                continue
+
+            elif name == "content" and isinstance(block, SnippetChooserBlock):
+                # Override the diff to display the diff of the revision's
+                # content you are comparing, not the pk of the snippet.
+                htmlvalues.append(self._get_revisions_diff())
+
+            else:
+                comparison_class = get_comparison_class_for_block(block)
+
+                htmlvalues.append(
+                    (
+                        label,
+                        comparison_class(
+                            block, True, True, val[name], val[name]
+                        ).htmlvalue(val[name]),
+                    )
+                )
+
+        return format_html(
+            "<dl>\n{}\n</dl>",
+            format_html_join(
+                "\n", "    <dt>{}</dt>\n    <dd>{}</dd>", htmlvalues
+            ),
+        )
+
     def htmldiff(self):
         htmldiffs = []
         for name, block in self.block.child_blocks.items():
@@ -30,29 +103,9 @@ class ReusableContentComparison(BlockComparison):
                 continue
 
             elif name == "content":
-                instance = self.val_a["content"]
-                revision_a = self.get_revision(self.val_a)
-                revision_b = self.get_revision(self.val_b)
-
-                # Copied from with modifications:
-                # https://github.com/wagtail/wagtail/blob/
-                # 9edaadaef094d7732025121c32313472b353ace8/
-                # wagtail/admin/views/generic/models.py#L739-L752
-                comparison = (
-                    get_edit_handler(self.val_a["content"])
-                    .get_bound_panel(instance=instance)
-                    .get_comparison()
-                )
-
-                for comp in comparison:
-                    diff = comp(revision_a, revision_b)
-                    if diff.has_changed():
-                        htmldiffs.append(
-                            (
-                                instance._meta.verbose_name,
-                                diff.htmldiff(),
-                            )
-                        )
+                # Override the diff to display the diff of the revision's
+                # content you are comparing, not the pk of the snippet.
+                htmldiffs.append(self._get_revisions_diff())
 
             else:
                 comparison_class = get_comparison_class_for_block(block)

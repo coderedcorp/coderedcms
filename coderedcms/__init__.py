a00ecbded4328b988dcf3e1aaed3a729fe35a62d
Jeremy <jchilders@coderedcorp.com>
2023-07-20 15:50:11 -0400

    Bump package version to 2.1.4

--- coderedcms/__init__.py
+++ coderedcms/__init__.py
@@ -14,7 +14,7 @@ Maintains version of coderedcms.
 
 See: https://www.python.org/dev/peps/pep-0440/
 """
-release = ["2", "1", "3", "", ""]
+release = ["2", "1", "4", "", ""]
 
 
 def _get_version() -> str:

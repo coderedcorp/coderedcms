6775b300474c62358c9e85116d6c08d013550396
Michael Steigman <michael@steigman.net>
2024-02-19 16:44:50 -0500

    removed mixin tests and simplified decorator tests

--- coderedcms/models/tests/test_wagtailsettings_models.py
+++ coderedcms/models/tests/test_wagtailsettings_models.py
@@ -7,9 +7,7 @@ from wagtail.models import Site
 from coderedcms.tests.testapp.models import WebPage
 from coderedcms.models.wagtailsettings_models import (
     AnalyticsSettings,
-    LayoutSettings,
     maybe_register_setting,
-    SettingsCheckMixin,
 )
 
 
@@ -64,46 +62,6 @@ class AnalyticsSettingsTestCase(WagtailPageTests):
         self.assertInHTML(self.settings.body_scripts, str(response.content), 1)
 
 
-class SettingsCheckTestCase(WagtailPageTests):
-
-    def setUp(self):
-        super().setUp()
-        self.site = Site.objects.get(is_default_site=True)
-        self.layout_settings = LayoutSettings.for_site(self.site)
-        self.analytics_settings = AnalyticsSettings.for_site(self.site)
-
-    def test_layoutsettings_default(self):
-        self.assertTrue(self.layout_settings.enabled())
-
-    @override_settings(CRX_ENABLE_LAYOUT_SETTINGS=True)
-    def test_layoutsettings_enabled(self):
-
-        self.assertTrue(self.layout_settings.enabled())
-
-    @override_settings(CRX_ENABLE_LAYOUT_SETTINGS=False)
-    def test_layoutsettings_disabled(self):
-
-        self.assertFalse(self.layout_settings.enabled())
-
-    def test_analyticssettings_default(self):
-
-        self.assertTrue(self.analytics_settings.enabled())
-
-    @override_settings(CRX_ENABLE_ANALYTICS_SETTINGS=True)
-    def test_analyticssettings_enabled(self):
-
-        self.assertTrue(self.analytics_settings.enabled())
-
-    @override_settings(CRX_ENABLE_ANALYTICS_SETTINGS=False)
-    def test_analyticssettings_disabled(self):
-
-        self.assertFalse(self.analytics_settings.enabled())
-
-
-class DummySettingsObject(SettingsCheckMixin):
-    ENABLE_SETTINGS = "CRX_ENABLE_LAYOUT_SETTINGS"
-
-
 class MaybeRegisterSettingTestCase(WagtailPageTests):
     """Testing the maybe_register_setting decorator.
 
@@ -113,10 +71,10 @@ class MaybeRegisterSettingTestCase(WagtailPageTests):
 
     def setUp(self):
         super().setUp()
-        self.dummy_settings = DummySettingsObject()
+        self.dummy_settings = object()
 
     @patch("coderedcms.models.wagtailsettings_models.register_setting")
-    def test_decorator_enabled(self, mock_register_setting):
+    def test_decorator_default(self, mock_register_setting):
         """Test that the decorator calls register_setting
         with no override setting (default)."""
         maybe_register_setting(icon="foobar")(self.dummy_settings)
@@ -124,7 +82,17 @@ class MaybeRegisterSettingTestCase(WagtailPageTests):
             self.dummy_settings, icon="foobar"
         )
 
-    @override_settings(CRX_ENABLE_LAYOUT_SETTINGS=False)
+    @override_settings(CRX_DISABLE_SITE_SETTINGS=False)
+    @patch("coderedcms.models.wagtailsettings_models.register_setting")
+    def test_decorator_enabled(self, mock_register_setting):
+        """Test that the decorator calls register_setting
+        when override setting is False."""
+        maybe_register_setting(icon="foobar")(self.dummy_settings)
+        mock_register_setting.assert_called_once_with(
+            self.dummy_settings, icon="foobar"
+        )
+
+    @override_settings(CRX_DISABLE_SITE_SETTINGS=True)
     @patch("coderedcms.models.wagtailsettings_models.register_setting")
     def test_decorator_disabled(self, mock_register_setting):
         """Test that the decorator does not call register_setting

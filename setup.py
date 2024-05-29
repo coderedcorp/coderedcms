38fffed46f2e4284a8ed6acadf9545bdc3f25398
Vince Salvino <salvino@coderedcorp.com>
2024-05-29 16:19:36 -0400

    Update Bootstrap to 5.3.3

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     install_requires=[
         "beautifulsoup4>=4.8,<4.13",  # should be the same as wagtail
         "django-eventtools==1.0.*",
-        "django-bootstrap5==23.4",
+        "django-bootstrap5==24.2",
         "Django>=4.2,<6.0",  # should be the same as wagtail
         "geocoder==1.38.*",
         "icalendar==5.0.*",

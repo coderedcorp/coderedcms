43fbc0d5e29a82287242485fd576a4dafb152afe
jacklinke <jacklinke@gmail.com>
2025-02-20 23:47:14 -0500

    Correct another linting issue

--- coderedcms/project_template/pro/project_name/settings/base.py
+++ coderedcms/project_template/pro/project_name/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/{{ docs_version }}/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / "subdir".
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
